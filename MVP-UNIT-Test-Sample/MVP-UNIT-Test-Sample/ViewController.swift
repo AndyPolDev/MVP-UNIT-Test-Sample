747306be513183cf1adc67f67ee8749d5f3822a1
AndyPolDev <AndyPolDev@gmail.com>
2023-01-20 13:24:09 +0800

    MVP-UNIT-Test-Sample - 001. Project created

---
+++ MVP-UNIT-Test-Sample/MVP-UNIT-Test-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-UNIT-Test-Sample
+//
+//  Created by ANDY on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
