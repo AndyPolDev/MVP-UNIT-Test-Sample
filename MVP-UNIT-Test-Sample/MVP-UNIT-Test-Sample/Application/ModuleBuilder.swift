9ff018aa0ea4d226bf656bdd950f3d3e14b9486d
AndyPolDev <AndyPolDev@gmail.com>
2023-01-24 21:58:59 +0800

    MVP-UNIT-Test-Sample - 012. Detail Module builder created

--- MVP-UNIT-Test-Sample/MVP-UNIT-Test-Sample/Application/ModuleBuilder.swift
+++ MVP-UNIT-Test-Sample/MVP-UNIT-Test-Sample/Application/ModuleBuilder.swift
@@ -2,6 +2,7 @@ import UIKit
 
 protocol Builder {
     static func createMainModule() -> UIViewController
+    static func createDetailModule(comment: Comment?) -> UIViewController
 }
 
 // Здесь как раз происходит внедрение зависимости извне, вместо view мы можем подсунуть mock view
@@ -15,4 +16,11 @@ class ModuleBuilder: Builder {
     }
     
     //Для каждого модуля мы пишем отельную функцию createSomeModule()
+    static func createDetailModule(comment: Comment?) -> UIViewController {
+        let view = DetailViewController()
+        let networkService = NetworkService()
+        let presenter = DetailPresenter(view: view, networkService: networkService, comment: comment)
+        view.presenter = presenter
+        return view
+    }
 }
